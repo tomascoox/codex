e55ad4c32266adc594d1101f49fd367e4e6cb7cd
Tomas Coox <tomas@joox.se>
2023-01-07 01:07:20 +0100

    changed to live server instead of localhost

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from the server-> bot's response
 
-  const response = await fetch('http://localhost:5001', {
+  const response = await fetch('https://codex-deup.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
