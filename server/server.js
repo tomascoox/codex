ba8ef1d7cb73479def5b77375e62fa132fde02e6
Tomas Coox <tomas@joox.se>
2023-01-10 15:25:39 +0100

    change bot-personality to a chatbot with sarcasm

--- server/server.js
+++ server/server.js
@@ -27,11 +27,11 @@ app.post('/', async (req, res) => {
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: `${prompt}`,
-            temperature: 0,
-            max_tokens: 3000,
-            top_p: 1,
-            frequency_penalty: 0.0,
-            presence_penalty: 0,
+            temperature: 0.5,
+            max_tokens: 60,
+            top_p: 0.3,
+            frequency_penalty: 0.5,
+            presence_penalty: 0.0,
         })
 
         res.status(200).send({
