efa131d66c07fbf30b40ce987790024181ffa412
Tomas Coox <tomas@joox.se>
2023-01-10 12:00:16 +0100

    reset bot-config to first

--- server/server.js
+++ server/server.js
@@ -27,11 +27,11 @@ app.post('/', async (req, res) => {
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: `${prompt}`,
-            temperature: 0.9,
+            temperature: 0,
             max_tokens: 3000,
-            top_p: 0.3,
-            frequency_penalty: 0.5,
-            presence_penalty: 0.0,
+            top_p: 1,
+            frequency_penalty: 0.0,
+            presence_penalty: 0,
             stop: ["\n"],
         })
 
