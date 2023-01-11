34006efa6a2e507be59112f47b508018a3ca9e76
Tomas Coox <tomas@joox.se>
2023-01-11 23:36:30 +0100

    implement context

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 })
 
+let context = {}
+
 const openai = new OpenAIApi(configuration)
 
 const app = express()
@@ -22,6 +24,7 @@ app.get('/', async (req, res) => {
 })
 
 app.post('/', async (req, res) => {
+    let userContext = context
     try {
         const prompt = req.body.prompt
         const response = await openai.createCompletion({
@@ -29,6 +32,7 @@ app.post('/', async (req, res) => {
             prompt: `${prompt}`,
             temperature: 0.5,
             max_tokens: 3000,
+            context: userContext,
             top_p: 0.3,
             frequency_penalty: 0.5,
             presence_penalty: 0.0,
@@ -37,6 +41,8 @@ app.post('/', async (req, res) => {
         res.status(200).send({
             bot: response.data.choices[0].text
         })
+        let newContext = response.data.choices[0].context
+        context = newContext
     } catch (error) {
         console.log(error)
         res.status(500).send({ error })
